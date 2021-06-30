f2899d9406d4a32cf8d660ca9dc4f5dd2f2a35b1
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2021-06-30 17:53:18 +0800

    chore: + chunk hash for css file

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ yarn build
 
 rsync -avzz --delete dist ${server}:~/
 
-ssh ${server} 'sudo mv /data/fe/rabby_prod ~/data/fe/rabby_prod$(date +%s) && sudo cp -r ~/dist /data/fe/rabby_prod/'
+ssh ${server} 'sudo cp -r ~/dist /data/fe/rabby_prod/'
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,27 @@
+const fs = require('fs');
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const tinypng = require('gulp-tinypng');
 const watch = require('gulp-watch');
 const runSequence = require('run-sequence');
+const rev = require('gulp-rev');
+const handlebars = require('gulp-compile-handlebars');
+const rename = require('gulp-rename');
 
 gulp.task('watch', function () {
   watch(['src/*.css','src/*.html'], function () {
-    runSequence(['minify-css', 'copy-html'])
+    runSequence(['minify-css', 'compile-index-html'])
   });
 });
 
 gulp.task('minify-css',function(){
 	return gulp.src('src/*.css')
     .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('copy-html',function(){
-	return gulp.src([
-		'src/*.html'])
-	.pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist'))
+		.pipe(rev())
+		.pipe(gulp.dest('dist'))
+		.pipe(rev.manifest())
+		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('copy-assets',function(){
@@ -29,6 +31,20 @@ gulp.task('copy-assets',function(){
 	.pipe(gulp.dest('dist/assets/'));
 });
 
-gulp.task('default', ['minify-css', 'copy-html', 'copy-assets']);
+const handlebarOpts = {
+	helpers: {
+		assetPath: (path, context) => context.data.root[path]
+	}
+};
+
+gulp.task('compile-index-html', function() {
+	const manifest = JSON.parse(fs.readFileSync('dist/rev-manifest.json', 'utf8'));
+	gulp.src('src/index.hbs')
+		.pipe(handlebars(manifest, handlebarOpts))
+		.pipe(rename('index.html'))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['minify-css', 'copy-assets', 'compile-index-html']);
 
 gulp.task('dev', ['watch'])
