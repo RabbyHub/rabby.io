36d3155bd3555bc9d9569d7aa292bfea62c295f2
Hodor <lost1q84@gmail.com>
2022-06-27 15:51:45 +0800

    feat: add new chain and brand (#10)

--- src/components/IntergrateChains.jsx
+++ src/components/IntergrateChains.jsx
@@ -5,7 +5,11 @@ const IntergrateChains = () => {
     { id: 1, name: "Ethereum", icon: "/assets/images/chain-logos/eth.svg" },
     { id: 56, name: "BSC", icon: "/assets/images/chain-logos/bsc.svg" },
     { id: 100, name: "Gnosis", icon: "/assets/images/chain-logos/gnosis.svg" },
-    { id: 137, name: "Polygon", icon: "/assets/images/chain-logos/polygon.svg" },
+    {
+      id: 137,
+      name: "Polygon",
+      icon: "/assets/images/chain-logos/polygon.svg",
+    },
     { id: 250, name: "Fantom", icon: "/assets/images/chain-logos/fantom.svg" },
     { id: 66, name: "OKC", icon: "/assets/images/chain-logos/okex.svg" },
     { id: 128, name: "HECO", icon: "/assets/images/chain-logos/heco.svg" },
@@ -53,11 +57,25 @@ const IntergrateChains = () => {
     { id: 8217, name: "Klaytn", icon: "/assets/images/chain-logos/klaytn.svg" },
     { id: 4689, name: "IoTeX", icon: "/assets/images/chain-logos/iotx.svg" },
     { id: 30, name: "RSK", icon: "/assets/images/chain-logos/rsk.svg" },
-    { id: 888, name: "Wanchain", icon: "/assets/images/chain-logos/wanchain.svg" },
+    {
+      id: 888,
+      name: "Wanchain",
+      icon: "/assets/images/chain-logos/wanchain.svg",
+    },
     { id: 321, name: "KCC", icon: "/assets/images/chain-logos/kcc.svg" },
-    { id: 19, name: "Songbird", icon: "/assets/images/chain-logos/songbird.png" },
+    {
+      id: 19,
+      name: "Songbird",
+      icon: "/assets/images/chain-logos/songbird.png",
+    },
     { id: 9001, name: "Evmos", icon: "/assets/images/chain-logos/evmos.svg" },
     { id: 53935, name: "DFK", icon: "/assets/images/chain-logos/dfk.svg" },
+    { id: 40, name: "Telos", icon: "/assets/images/chain-logos/telos.svg" },
+    {
+      id: 73772,
+      name: "Swimmer",
+      icon: "/assets/images/chain-logos/swimmer.svg",
+    },
   ];
   return (
     <div className="chains">
