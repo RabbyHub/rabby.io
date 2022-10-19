8a75e66318d9d3483f21c2489bfbeb5c47288ba7
qingwei-li <cinwell.li@gmail.com>
2022-10-19 11:08:51 +0800

    fix: tag

--- packages/claims/components/Confirm/UncancelledTip.tsx
+++ packages/claims/components/Confirm/UncancelledTip.tsx
@@ -4,7 +4,7 @@ import { PostClaimButton } from '../PostClaimButton';
 export const UncancelledTip = () => {
   return (
     <div className="text-center px-6">
-      <p className="text-[#EC5151] text-sm mb-7 max-w-[520px] m-auto flex ">
+      <div className="text-[#EC5151] text-sm mb-7 max-w-[520px] m-auto flex">
         <img className="w-5 mr-3" src={`${BASE_PATH}/info.svg`} alt="" />
         <div className="text-left">
           The current address has uncanceled approvals for Rabby Swap.{' '}
@@ -15,7 +15,7 @@ export const UncancelledTip = () => {
             Please revoke approvals first to continue the claim process.
           </a>
         </div>
-      </p>
+      </div>
 
       <PostClaimButton disabled />
     </div>
