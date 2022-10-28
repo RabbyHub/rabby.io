f592856de8a4405bb3575ca71e91924fcc52508c
DMY <147dmy@gmail.com>
2022-10-28 15:00:31 +0800

    feat: metamask export (#20)
    * feat: metamask export
    
    * chore: text updates

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import ReactGA from 'react-ga';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import ReactGA from "react-ga";
 
-ReactGA.initialize('UA-199755108-2');
+import { MetaMaskExport } from "./page/metamask";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-ReactGA.pageview('/');
+ReactGA.initialize("UA-199755108-2");
+
+ReactGA.pageview("/");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Route index element={<App />} />
+          <Route path="/metamask-export" element={<MetaMaskExport />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
