4f1a8ba864d70a68fef0cc750564b1616efefa0a
DMY <147dmy@gmail.com>
2022-11-14 16:09:51 +0800

    doc: update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 module.exports = {
   base: '/docs/',
-  title: 'Rabby',
+  title: 'Debank & Rabby',
   description: 'Document for Rabby',
   nextLinks: false,
   prevLinks: false,
   dest: path.resolve(__dirname, '../../dist/docs'),
   themeConfig: {
-    logo: '/logo.png'
+    logo: '/debank-512.png'
   },
   head: [
     ['link', { rel: 'icon', href: `https://rabby.io/assets/images/favicon.png` }]
