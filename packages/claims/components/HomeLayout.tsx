4e7d0a3ea502e087af057a7ccdd7dcb6f89e8d35
heisenberg <heisenberg-2077@outlook.com>
2022-12-02 13:15:30 +0800

    feat: offline claim

--- packages/claims/components/HomeLayout.tsx
+++ packages/claims/components/HomeLayout.tsx
@@ -3,6 +3,7 @@ import { useAccount } from 'wagmi';
 import React from 'react';
 import { ClaimsSubmissionGuidelinesCard } from './Content/ClaimsSubmissionGuidelinesCard';
 import { ConfirmCard } from './Confirm/ConfirmCard';
+import { Card } from './Card';
 
 export const HomeLayout = () => {
   const [mounted, setMounted] = React.useState(false);
@@ -26,6 +27,12 @@ const WalletPanel = () => {
   return isConnected ? (
     <ConfirmCard />
   ) : (
-    <WalletButton className="mt-4 md:mt-[80px]" />
+    // <WalletButton className="mt-4 md:mt-[80px]" />
+    <Card className="mt-4 font-bold">
+      Claim submission has ended. If you have any questions, please contact{' '}
+      <a className="underline" href="mailto:rabby@debank.com">
+        rabby@debank.com
+      </a>
+    </Card>
   );
 };
