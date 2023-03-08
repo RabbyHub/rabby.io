3de1dd7c8be7a1eeccdb2644bc0b719fc97b9170
heisenberg <heisenberg-2077@outlook.com>
2023-03-08 14:18:12 +0800

    refactor: remove claims

--- packages/claims/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/antd/index.less';
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
