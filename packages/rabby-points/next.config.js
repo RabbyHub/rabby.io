d0ebd5f74b30e03ac44fb2964a172fee7fddeabd
DMY <147dmy@gmail.com>
2024-01-22 04:37:09 +0000

    points

---
+++ packages/rabby-points/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/rabby-points",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
