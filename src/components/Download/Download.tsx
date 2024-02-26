78132cdd62f8d4662727a883791d13bbeae57d8b
richardo2016x <richardo2016x@gmail.com>
2024-02-27 05:00:05 +0800

    feat: support android apk download.

--- src/components/Download/Download.tsx
+++ src/components/Download/Download.tsx
@@ -80,6 +80,12 @@ export const Download: React.FC = () => {
                   report="Google Play"
                 />
                 <DownloadCard
+                  title="Android APK"
+                  icon="/assets/download/icon-android-apk.svg"
+                  href={MOBILE_DOWNLOAD_URL.selfHost}
+                  report="Android APK"
+                />
+                <DownloadCard
                   title="App Store"
                   icon="/assets/download/icon-app-store.png"
                   href=""
--- src/components/Download/mobile.ts
+++ src/components/Download/mobile.ts
@@ -1,4 +1,5 @@
 export const MOBILE_DOWNLOAD_URL = {
   googlePlay:
     "https://play.google.com/store/apps/details?id=com.debank.rabbymobile",
+  selfHost: "https://download.rabby.io/downloads/wallet-mobile/android/rabby-mobile.apk"
 };
