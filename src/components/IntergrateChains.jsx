94de37383dec259868ece9d6f029b965f12edb32
DMY <147dmy@gmail.com>
2024-04-01 15:23:20 +0800

    fix

--- src/components/IntergrateChains.jsx
+++ src/components/IntergrateChains.jsx
@@ -2,8 +2,9 @@ const IntergrateChains = ({ chains }) => {
   return (
     <div className="chains">
       <h2 className="title">Rabby has integrated {chains?.length} chains
-      <a className='goto-dashboard' href='/chain-dashboard' target='_blank' rel="noreferrer">View Chain Dashboard</a>
-      
+      <a className='goto-dashboard' href='/chain-dashboard' target='_blank' rel="noreferrer">View Chain Dashboard
+      <img src='/assets/chain-dashboard/arrow-right.svg' alt=''/>
+      </a>
       </h2>
       <ul className="chain-list">
         {chains?.map((chain) => (
