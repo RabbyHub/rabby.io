c7b5f651db574f284dbbd67731a5b95c50ac9105
DMY <147dmy@gmail.com>
2024-10-10 10:30:55 +0800

    docs: add Term of Use;update Privacy (#82)
    * docs: add Term of Use;update Privacy
    
    * update footer css
    
    * update style

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  base: '/docs/',
-  title: 'Debank & Rabby',
-  description: 'Document for Rabby',
+  base: "/docs/",
+  title: "Debank & Rabby",
+  description: "Document for Rabby",
   nextLinks: false,
   prevLinks: false,
-  dest: path.resolve(__dirname, '../../dist/docs'),
+  dest: path.resolve(__dirname, "../../dist/docs"),
   themeConfig: {
-    logo: '/debank-512.png'
+    logo: "/debank-512.png",
+    navbar: false,
   },
+
   head: [
-    ['link', { rel: 'icon', href: `https://rabby.io/assets/images/favicon.png` }]
+    [
+      "link",
+      { rel: "icon", href: `https://rabby.io/assets/images/favicon.png` },
+    ],
   ],
-}
+};
