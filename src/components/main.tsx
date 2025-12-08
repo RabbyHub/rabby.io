3cb3564655837fb6a2f06fc75b50479a6dfbea14
vvvvvv1vvvvvv <86296331+vvvvvv1vvvvvv@users.noreply.github.com>
2025-12-08 19:20:54 +0800

    feat: set desktop version in development (#104)
    * feat: set desktop version in development
    
    * feat: rm footer download
    
    * fix: cursor
    
    * feat: update icon

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import styles from "./Download/style.module.scss";
 import { DownloadButton } from "./Download/DownloadButton";
-import { DOWNLOAD_INFO, BROWSER_DOWNLOAD_INFO, DownloadType, DOWNLOAD_INFO_MOBILE } from "./Download/download-info";
+import {
+  DOWNLOAD_INFO,
+  BROWSER_DOWNLOAD_INFO,
+  DownloadType,
+  DOWNLOAD_INFO_MOBILE,
+} from "./Download/download-info";
 import { DownloadIcon } from "./Download/DownloadIcon";
 import { DownloadMobileButton } from "./Download/DownloadMobileButton";
 import { useIsSmallScreen } from "../hooks/useIsSmallScreen";
@@ -11,84 +16,102 @@ import clsx from "clsx";
 const detectBrowser = () => {
   const userAgent = navigator.userAgent.toLowerCase();
   if (/brave/i.test(userAgent)) {
-    return 'brave';
+    return "brave";
   } else if (/edg\//i.test(userAgent)) {
-    return 'edge';
+    return "edge";
   }
-  return 'chrome';
+  return "chrome";
 };
 
 const detectMobileOS = () => {
   const userAgent = navigator.userAgent.toLowerCase();
   if (/android/i.test(userAgent)) {
-    return 'googlePlay';
+    return "googlePlay";
   }
-  return 'appStore';
+  return "appStore";
 };
 
 export const Main: React.FC = () => {
-    const isSmallScreen = useIsSmallScreen();
-    const currentBrowser = detectBrowser();
-    const browserInfo = BROWSER_DOWNLOAD_INFO[currentBrowser as keyof typeof BROWSER_DOWNLOAD_INFO];
-    const currentMobile = detectMobileOS();
-    const [activeKey, setActiveKey] = useState<string | null>(null);
+  const isSmallScreen = useIsSmallScreen();
+  const currentBrowser = detectBrowser();
+  const browserInfo =
+    BROWSER_DOWNLOAD_INFO[currentBrowser as keyof typeof BROWSER_DOWNLOAD_INFO];
+  const currentMobile = detectMobileOS();
+  const [activeKey, setActiveKey] = useState<string | null>(null);
 
-    return (
+  return (
     <div className={styles.main}>
-        <div className={styles.buttonGroup}>
-                {isSmallScreen ? (
-                    <button className={clsx(styles.downloadMobileButtonSmall)} onClick={() => {
-                        window.open(currentMobile === 'googlePlay' ? DOWNLOAD_INFO_MOBILE.googlePlay.href : DOWNLOAD_INFO_MOBILE.appStore.href, '_blank');
-                    }}>
-                        <img alt="download" src={currentMobile === 'googlePlay' ? '/assets/download/google-play-blue.svg' : '/assets/download/apple-store-blue.svg'}/>
-                        <div>Download for {currentMobile === 'googlePlay' ? 'Android' : 'iOS'}</div>
-                    </button>
-                ) : (
-                <>
-                    <DownloadButton
-                        className={clsx(styles.downloadBrowserButton)}
-                        title={`Download for ${browserInfo.title}`}
-                        icon={browserInfo.icon}
-                        href={browserInfo.href}
-                        report={browserInfo.title}
-                    />
-                    <DownloadMobileButton className={styles.downloadMobileButton} />
-                </>
-            )} 
-        </div>
-        <div className={styles.downloadTips}>
-            Also available on other browsers and devices
-        </div>
-        <div className={styles.downloadList}>
-            {Object.entries(DOWNLOAD_INFO).map(([key, info]) => {
-                if (isSmallScreen) {
-                    if (info.type === DownloadType.APP && key === currentMobile) {
-                        return null;
-                    }
-                } else {
-                    if (info.type === DownloadType.BROWSER && key === currentBrowser) {
-                        return null;
-                    }   
-                }
+      <div className={styles.buttonGroup}>
+        {isSmallScreen ? (
+          <button
+            className={clsx(styles.downloadMobileButtonSmall)}
+            onClick={() => {
+              window.open(
+                currentMobile === "googlePlay"
+                  ? DOWNLOAD_INFO_MOBILE.googlePlay.href
+                  : DOWNLOAD_INFO_MOBILE.appStore.href,
+                "_blank"
+              );
+            }}
+          >
+            <img
+              alt="download"
+              src={
+                currentMobile === "googlePlay"
+                  ? "/assets/download/google-play-blue.svg"
+                  : "/assets/download/apple-store-blue.svg"
+              }
+            />
+            <div>
+              Download for {currentMobile === "googlePlay" ? "Android" : "iOS"}
+            </div>
+          </button>
+        ) : (
+          <>
+            <DownloadButton
+              className={clsx(styles.downloadBrowserButton)}
+              title={`Download for ${browserInfo.title}`}
+              icon={browserInfo.icon}
+              href={browserInfo.href}
+              report={browserInfo.title}
+            />
+            <DownloadMobileButton className={styles.downloadMobileButton} />
+          </>
+        )}
+      </div>
+      <div className={styles.downloadTips}>
+        Also available on other browsers and devices
+      </div>
+      <div className={styles.downloadList}>
+        {Object.entries(DOWNLOAD_INFO).map(([key, info]) => {
+          if (isSmallScreen) {
+            if (info.type === DownloadType.APP && key === currentMobile) {
+              return null;
+            }
+          } else {
+            if (info.type === DownloadType.BROWSER && key === currentBrowser) {
+              return null;
+            }
+          }
 
-                return (
-                    <DownloadIcon
-                        key={key}
-                        infoKey={key}
-                        title={info.title}
-                        src={info.icon}
-                        alt={info.title}
-                        href={info.href}
-                        type={info.type}
-                        className={styles.downloadIcon}
-                        active={activeKey === key}
-                        onMouseEnter={() => setActiveKey(key)}
-                        onMouseLeave={() => setActiveKey(null)}
-                        disableHover={isSmallScreen}
-                    />
-                );
-            })}
-        </div>
+          return (
+            <DownloadIcon
+              key={key}
+              infoKey={key}
+              title={info.title}
+              src={info.icon}
+              alt={info.title}
+              href={info.href}
+              type={info.type}
+              className={styles.downloadIcon}
+              active={activeKey === key}
+              onMouseEnter={() => setActiveKey(key)}
+              onMouseLeave={() => setActiveKey(null)}
+              disableHover={isSmallScreen}
+            />
+          );
+        })}
+      </div>
     </div>
-    )
-}
+  );
+};
